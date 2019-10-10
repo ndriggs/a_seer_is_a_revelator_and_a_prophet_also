c79765205907fee30ff06a34266e3550e6db55f1
ndriggs <nathanieldriggs@gmail.com>
2019-10-10 22:33:43 +0000

    I did it!

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <set>
 #include <vector>
+#include <list>
 #include <map>
 
 using namespace std;
@@ -62,16 +63,86 @@ int main(int argc, char *argv[]){
         
         //put vector of words into a map or strings and string
         map<string, string> firstmap;
-        vector<string>::iterator it = scripture_words_v.begin();
         string last = "";
-        for(it != scripture_words_v.end()){
+        for(vector<string>::iterator it = scripture_words_v.begin(); it != scripture_words_v.end(); ++it){
             firstmap[last] = *it;
             last = *it;
-            ++it;
         }
         //output map to file
         ofstream map_output(file_name + "_1_1.txt");
-        for(map<string, string>::iterator it = firstmap.begin(); it !=)
+        for(map<string, string>::iterator it = firstmap.begin(); it != firstmap.end(); ++it){
+            map_output << it->first << ", " << it->second << endl;
+        }
+        map_output.close();
+        
+        //creating scripture, take 1
+        string state = "";
+        cout << endl << "Take 1!" << endl;
+        for(int i = 0; i < 100; i++){
+            cout << firstmap[state] << " ";
+            state = firstmap[state];
+        }
+        cout << endl; // clear the buffer
+        
+        //scripture map 2.0
+        map<string, vector<string>> map2point0;
+        state = "";
+        for(vector<string>::iterator it = scripture_words_v.begin(); it != scripture_words_v.end(); ++it){
+            map2point0[state].push_back(*it);
+            state = *it;
+        }
+        /** 
+        //print out vector corresponding to 6th entry in map
+        for(map<string, vector<string>>::iterator it = map2point0.begin(); it != map2point0.end(); ++it){
+            int i = 0;
+            if(i = 5){ //access 6th entry
+                for(int j = 0; j < it->second.size(); j++){
+                    cout << it->second[j] << ", ";
+                }
+                cout << endl; //clear the buffer
+            } 
+            i += 1;
+        }**/
+        
+        //creating scripture --- take 2!
+        srand(time(NULL)); //initialize random rumber generator
+        state = "";
+        cout << endl << "Version 2: " << endl;
+        for(int i = 0; i < 100; i++){
+           int index = rand() % map2point0[state].size();
+           state = map2point0[state][index];
+           cout << state << " ";
+        }
+        cout << endl;
+        
+        //let's use a map of a list and vector!
+        const int M = 2;
+        map<list<string>, vector<string>> map3point0;
+        list<string> context;
+        for(int i = 0; i < M; i++){
+            context.push_back("");
+        }
+        //add some context to our new map
+        for(vector<string>::iterator it = scripture_words_v.begin(); it != scripture_words_v.end(); ++it){
+            map3point0[context].push_back(*it);
+            context.push_back(*it);
+            context.pop_front();
+        }
+        
+        //now let's print it~ we're publishing baby!
+        context.clear();
+        for(int i = 0; i < M; i++){
+            context.push_back("");
+        }
+        cout << endl << "The best one yet!" << endl;
+        for(int i = 0; i < 100; i++){
+            int index = rand() % map3point0[context].size();
+            cout << map3point0[context][index] << " ";
+            context.push_back(map3point0[context][index]);
+            context.pop_front();
+        }
+        
+        
     }
     
     
